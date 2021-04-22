7d2da82b5b67c7e86c678a63a235a4333e98491d
sachin kumar <sachin798711@gmail.com>
2021-04-22 20:23:46 +0530

    aa

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-   git push -f git@github.com:sachin798711/counttext.live.io.git master:gh-pages
+ git push -f git@github.com:sachin798711/counttext.live.io.git master:gh-pages
 
 cd -
-
- 
