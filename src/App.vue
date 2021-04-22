6323149764cd0976f632b5f9420abb724407affc
sachin kumar <sachin798711@gmail.com>
2021-04-22 16:15:57 +0530

    live

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!---<HelloWorld msg="Welcome to Your Vue.js App"/> --->
+    <Count />
+    
+  </div>
+</template>
+
+
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Count  from './components/Count.vue'
+
+
+export default {
+  name: 'App',
+
+  components: {
+    //HelloWorld,
+    Count,
+  
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0176eb;
+  margin-top: 60px;
+  
+
+}
+</style>
---
+++ src/components/Count.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>   
+   <h1>Hello My Name is Sachin Kumar </h1>
+   <p>Enter The Text Below <p>
+     <p>Hello This Is a Charecter Count Text box<p>
+<input v-model="message" placeholder="Enter The Text Below Charecter Count">
+<p>Message is: {{ message.length }}</p>
+
+
+ </div>
+  
+
+
+
+</template>
+<script>
+export default {
+  name: 'Count',
+         data(){
+           return {
+             message: ""
+           }
+         }
+}
+</script>
+
+
+
